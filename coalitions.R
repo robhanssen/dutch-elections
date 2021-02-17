400f78dac4cb702b8957feed05f55b5f6f7e805f
Rob Hanssen <rob.hanssen@outlook.com>
2021-02-17 16:27:06 -0500

    update on combat function

--- coalitions.R
+++ coalitions.R
@@ -1,12 +1,12 @@
 library(tidyverse)
 library(R.utils)
-#library(plyr)
 
 # poll Feb 16
 election = tibble(
                   party = c("VVD","PVV","CDA","D66","GL","SP","PvdA","CU","PvdD","50PLUS","SGP","Denk","FvD", "JA21"),
                   seats = c( 38,    22,   20,  15,   11,  10,   12,   6,   7,       1,       2,     2,   3,     1),
-     distancefromcenter = c(  -4,   -9,    -2,   2,   6,   8,    3,   -4,   7,      -2,     -8,     5,   -7,    5)
+                  # ranked mainly on right-left balance in Dutch politics; PVV and FvD are arbitrarily off because of cordon sanitaire
+     distancefromcenter = c(  -4,   -15,  -2,   2,   6,   8,    3,   -4,   7,      -2,      -8,     5, -15,     5)
                   ) %>%
             filter(seats > 0) %>%
             arrange(-seats) %>%
@@ -25,42 +25,43 @@ coalitions = tibble(
                     stability = 0
                     ) 
 
-coalitions$partylist = sapply(numbers, function(i) 
+partycompatibility <- function(pvec, elec)
+{
+    numseatbyparty = pvec*election$seats
+    repvec = rep(elec$distancefromcenter, numseatbyparty)
+    sd(repvec)
+}
+
+resultlist = sapply(numbers, function(i) 
                                             { pv = as.integer(unlist(strsplit(coalitions$bitcode[i], split="")))
-                                              outv=""
+                                              # generate the party list by matching "1"s to names
+                                              partylist = ""
                                               for(j in 1:length(election$party)) 
                                                   { 
                                                     if(pv[j]==1) 
-                                                        outv = paste(outv,election$party[j])     
+                                                        partylist = paste(partylist, election$party[j])     
                                                   } 
-                                              outv
+                                              # count number of "1"s
+                                              numparties = sum(pv)
+                                              # count number of seats in total set
+                                              seatcount = sum(pv*election$seats)
+                                              # esotheric compatibility function
+                                              #compat = sd((pv*election$distancefromcenter)[pv*election$distancefromcenter!=0])
+                                              compat = partycompatibility(pv, election)
+                                              # output list
+                                              list(partylist, numparties, seatcount, compat)
                                             }
                               ) 
 
-coalitions$numparties = sapply(numbers, function(i) 
-                                            { pv = as.integer(unlist(strsplit(coalitions$bitcode[i], split="")))
-                                              parties = sum(pv)
-                                            }
-                              ) 
+coalitions$partylist = unlist(resultlist[1,])
+coalitions$numparties = unlist(resultlist[2,])
+coalitions$seatcount = unlist(resultlist[3,])
+coalitions$compat = unlist(resultlist[4,])
+coalitions$stability = 1/((coalitions$numparties)^.5*(coalitions$compat)^.5)
 
-coalitions$seatcount = sapply(numbers, function(i) 
-                                            { pv = as.integer(unlist(strsplit(coalitions$bitcode[i], split="")))
-                                              parties = sum(pv*election$seats)
-                                            }
-                              ) 
-
-coalitions$compat = sapply(numbers, function(i) 
-                                            { pv = as.integer(unlist(strsplit(coalitions$bitcode[i], split="")))
-                                              pv2 = (pv*election$distancefromcenter)[pv*election$distancefromcenter!=0]
-                                              parties = sd(pv2)
-                                            }
-                              ) 
-
-coalitions$stability = -log((coalitions$numparties)^.6*(coalitions$compat)^.4)
-
-majoritycoalitions <- coalitions %>% filter(seatcount>=75) %>% 
-                                     filter(numparties <=5 ) %>% 
-                                     arrange(-stability, numparties,-seatcount) %>% 
+majoritycoalitions <- coalitions %>% filter(seatcount>= 75) %>% 
+                                     filter(numparties <= 5 ) %>% 
+                                     arrange(-stability, -seatcount, numparties) %>% 
                                      select(-numbers,-bitcode)
 
 minoritycoalitions <- coalitions %>% filter(seatcount>=70) %>% 
@@ -70,17 +71,10 @@ minoritycoalitions <- coalitions %>% filter(seatcount>=70) %>%
                                      select(-numbers,-bitcode)
 
 minorityplus <- coalitions %>% filter(seatcount>=65) %>% 
-                                     #filter(seatcount<75) %>% 
                                      filter(numparties <=6 ) %>% 
                                      arrange(-stability,numparties,-seatcount) %>% 
                                      select(-numbers,-bitcode)
 
-
 View(majoritycoalitions)
 View(minoritycoalitions)
 View(minorityplus)
-
-pv = as.integer(unlist(strsplit(coalitions$bitcode[8192], split="")))
-p1 = sd((pv*election$distancefromcenter)[pv*election$distancefromcenter!=0])
-p2 = p1[p1!=0]
-parties = sd(p2)
