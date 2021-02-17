819c1919340cfd1e359ddbb78df70a6e043139fb
Rob Hanssen <rob.hanssen@outlook.com>
2021-02-17 14:24:21 -0500

    coalition preference calculation

--- coalitions.R
+++ coalitions.R
@@ -6,7 +6,7 @@ library(R.utils)
 election = tibble(
                   party = c("VVD","PVV","CDA","D66","GL","SP","PvdA","CU","PvdD","50PLUS","SGP","Denk","FvD", "JA21"),
                   seats = c( 38,    22,   20,  15,   11,  10,   12,   6,   7,       1,       2,     2,   3,     1),
-     distancefromcenter = c(  -3,   -9,    -2,   2,   4,   8,    3,   -5,   7,      -2,     -7,     5,   -7,    5)
+     distancefromcenter = c(  -4,   -9,    -2,   2,   6,   8,    3,   -4,   7,      -2,     -8,     5,   -7,    5)
                   ) %>%
             filter(seats > 0) %>%
             arrange(-seats) %>%
@@ -51,11 +51,12 @@ coalitions$seatcount = sapply(numbers, function(i)
 
 coalitions$compat = sapply(numbers, function(i) 
                                             { pv = as.integer(unlist(strsplit(coalitions$bitcode[i], split="")))
-                                              parties = sd(pv*election$distancefromcenter)
+                                              pv2 = (pv*election$distancefromcenter)[pv*election$distancefromcenter!=0]
+                                              parties = sd(pv2)
                                             }
                               ) 
 
-coalitions$stability = -log((coalitions$numparties)^.4*(coalitions$compat)^.6)
+coalitions$stability = -log((coalitions$numparties)^.6*(coalitions$compat)^.4)
 
 majoritycoalitions <- coalitions %>% filter(seatcount>=75) %>% 
                                      filter(numparties <=5 ) %>% 
@@ -63,12 +64,23 @@ majoritycoalitions <- coalitions %>% filter(seatcount>=75) %>%
                                      select(-numbers,-bitcode)
 
 minoritycoalitions <- coalitions %>% filter(seatcount>=70) %>% 
-                                     filter(seatcount<=75) %>% 
+                                     filter(seatcount<75) %>% 
                                      filter(numparties <=5 ) %>% 
-                                     arrange(numparties,-seatcount) %>% 
+                                     arrange(-stability,numparties,-seatcount) %>% 
+                                     select(-numbers,-bitcode)
+
+minorityplus <- coalitions %>% filter(seatcount>=65) %>% 
+                                     #filter(seatcount<75) %>% 
+                                     filter(numparties <=6 ) %>% 
+                                     arrange(-stability,numparties,-seatcount) %>% 
                                      select(-numbers,-bitcode)
 
 
 View(majoritycoalitions)
+View(minoritycoalitions)
+View(minorityplus)
 
-majoritycoalitions %>% ggplot+aes(x=stability) + geom_density()
+pv = as.integer(unlist(strsplit(coalitions$bitcode[8192], split="")))
+p1 = sd((pv*election$distancefromcenter)[pv*election$distancefromcenter!=0])
+p2 = p1[p1!=0]
+parties = sd(p2)
